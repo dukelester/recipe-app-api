c3375509f6fff0909dfaaf5fb1cc5151a4475d0b
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-12-04 00:30:20 +0300

    Create the model for the tag

--- app/core/models.py
+++ app/core/models.py
@@ -67,8 +67,20 @@ class Recipe(models.Model):
     time_in_minutes = models.IntegerField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
     link = models.CharField(max_length=255)
+    tags = models.ManyToManyField('Tag')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.title
+
+
+class Tag(models.Model):
+    ''' The tag model '''
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
