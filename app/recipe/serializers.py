d604dec3975f7905a9cccce16b7eb756f3616c7d
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-12-03 14:04:47 +0300

    added the serializers for the recipe

---
+++ app/recipe/serializers.py
@@ -0,0 +1,18 @@
+'''
+Serializers for the APIS
+'''
+
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    ''' Serializer for the Recipe '''
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 'title', 'time_in_minutes'
+            'price', 'link', 'description',
+                ]
+        read_only_fields = ['id']
