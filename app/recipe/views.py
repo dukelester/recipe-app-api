88b77e9c134e33098adefd270e79cb1b3b943b15
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-12-10 13:20:04 +0300

    Updated the serializer

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,8 +1,10 @@
 ''' The Recipe Views '''
 
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from core.models import Recipe, Tag, Ingredient
 from . import serializers
@@ -43,11 +45,23 @@ class RecipeViewSet(viewsets.ModelViewSet):
         ''' Return the serializer to be used based on the action'''
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
         return self.serializer_class
 
     def perform_create(self, serializer):
         ''' Create a new recipe'''
         serializer.save(user=self.request.user)
+
+    @action(method=['POST'], detail=True, url_path='upload_image')
+    def upload_image(self, request, pk=None):
+        ''' Upload the image to recipe '''
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
